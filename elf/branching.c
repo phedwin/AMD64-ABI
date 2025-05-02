80d06836aec3d540be158df3ab067d005a2f41bf
phedwin <phedwinsching@students.uonbi.ac.ke>
2025-05-02 15:44:43 +0300

    branching [ direct x indirect branch] c alignment
    Signed-off-by: phedwin <phedwinsching@students.uonbi.ac.ke>

---
+++ elf/branching.c
@@ -0,0 +1,56 @@
+
+
+// TODO  THIS!
+#include <stdio.h>
+#include <stdlib.h>
+struct Vertex4D {};
+
+struct Rectangle {
+	int width, length;
+};
+
+#define CAST_TO_INT(x) ((int *)x)
+
+int area(void *values) {
+	int *first, results = 0;
+
+	first = CAST_TO_INT(values);
+
+	if (values)
+		results = *first * first[1];
+	return results;
+}
+struct Window {
+	int (*calculate_area)(void * /*keep this generic too */);
+	int (*calculate_perimeter)(void * /*struct Rectangle*/);
+};
+
+// default resolution
+static int width = 20;
+static int length = 40;
+
+int main(int a, char **p) {
+	struct Rectangle rectangle = {
+		.length = length,
+		.width = width,
+	};
+
+	// adjust
+	if (a > 2) {
+		rectangle.length = atoi(p[1]);
+		rectangle.width = atoi(p[2]);
+	}
+	struct Window *windows = malloc(sizeof(*windows));
+
+	if (windows == 0)
+		return 0;
+	/*! wait is this branching[ direct or indirect braching ] ? &
+	 * does it like change how the hardware behaves*/
+
+	windows->calculate_area = area;
+	int px = windows->calculate_area(&rectangle);
+
+	printf("window area = %d\n", px);
+
+	free(windows);
+}

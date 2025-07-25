3828e95af36fdc653034ad47ef5544ce1818143b
phedwin <phedwinsching@students.uonbi.ac.ke>
2025-07-25 23:01:09 +0300

    elf
    Signed-off-by: phedwin <phedwinsching@students.uonbi.ac.ke>

--- elf/elf.c
+++ elf/elf.c
@@ -13,19 +13,26 @@ typedef void NO_WHERE;
 
 #define DEBUGGER(...) fprintf(stderr, __VA_ARGS__)
 
-#if defined(__linux) && defined(__x86_64__) || (__i386__)
-
 #include <elf.h>
-NO_WHERE main(int argc, char **argv) {
-	int fd = open("image.bin", O_RDWR);
+void main(int argc, char **argv) {
+	int fd = open("image.bin", O_RDONLY);
 	if (fd < 0)
 		return;
+#if defined(__linux__) && defined(__x86_64__)
+	Elf64_Ehdr *elf = malloc(sizeof(Elf64_Ehdr));
+	if (elf == NULL)
+		return;
+	read(fd, elf, sizeof(Elf64_Ehdr));
+	DEBUGGER("size %d\n", elf->e_ehsize);
+
+/* this also depends on the elf being opened to be compiled with gcc -m32 */
+#elif (__i386__) && (__linux__)
 	Elf32_Ehdr *elf = malloc(sizeof(Elf32_Ehdr));
-	if (elf == 0)
+	if (elf == NULL)
 		return;
 	read(fd, elf, sizeof(Elf32_Ehdr));
-	printf("%d\n", elf->e_type);
-	free(elf);
-	return;
-}
+	DEBUGGER("size %d\n", elf->e_ehsize);
 #endif
+	free(elf);
+	exit(EXIT_SUCCESS);
+}
